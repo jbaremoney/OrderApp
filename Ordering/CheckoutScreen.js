fc2ea6b01ffe101ebb27e649052eb2dfdc658cfa
jbaremoney <114310093+jbaremoney@users.noreply.github.com>
2024-07-21 19:05:14 -0500

    working credit card payments through stripe

--- Ordering/CheckoutScreen.js
+++ Ordering/CheckoutScreen.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
+import axios from 'axios';
 import useCartStore from './CartManage';
 import { db, auth } from '../firebaseStuff/firebaseConfig';
 import { collection, addDoc, doc, Timestamp } from 'firebase/firestore';
-
-import axios from 'axios';
+import { initStripe, useStripe } from '@stripe/stripe-react-native';
 
 const CheckoutScreen = ({ navigation }) => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
   const [tip, setTip] = useState('');
-  // const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
   
-  const cart = useCartStore((state) => state.cart);
-  const barId = useCartStore((state) => state.bar);
-  const getSubtotal = useCartStore((state) => state.getSubtotal);
-  const subtotal = getSubtotal(cart);
+  const cart = useCartStore((state) => state.cart); // accessing cart attribute of zustand
+  const barId = useCartStore((state) => state.bar); // getting bar id to know where to send order
+  const getSubtotal = useCartStore((state) => state.getSubtotal);  // accessing get subtotal method of zustand
+  const subtotal = getSubtotal(cart);  // set subtotal const using getSubtotal method above
 
-  const TAXRATE = 0.05;
+  const TAXRATE = 0.05; // change this
   const taxAmount = TAXRATE * subtotal;
+
+  useEffect(() => {
+    // Initialize Stripe
+    initStripe({
+      publishableKey: 'pk_test_51PcwD52KnQ1b6e5XoMZCvpPMvTudZyVPnjZo7r1X8tS0eeWAferDYSF221E8hp7IO1jEUh6CrV2WS799aUs5dnah00BVY4tj1u',
+    });
+  }, []);
 
   const paymentMethods = [
     { id: 1, name: 'Credit Card' },
@@ -30,14 +37,18 @@ const CheckoutScreen = ({ navigation }) => {
   };
 
   const handleTipChange = (value) => {
+    // Remove non-numeric characters except dot
     const cleanedText = value.replace(/[^0-9.]/g, '');
+    
+    // Split the text into integer and decimal parts
     const parts = cleanedText.split('.');
     let formattedText = cleanedText;
-
+    
+    // Ensure there's at most one dot and format decimals
     if (parts.length > 1) {
       const integerPart = parts[0];
       let decimalPart = parts[1];
-      decimalPart = decimalPart.slice(0, 2);
+      decimalPart = decimalPart.slice(0, 2); // Limit decimal part to two digits
       formattedText = `${integerPart}.${decimalPart}`;
     }
 
@@ -49,37 +60,13 @@ const CheckoutScreen = ({ navigation }) => {
     return subtotal + taxAmount + tipAmount;
   };
 
-  // const fetchPaymentSheetParams = async () => {
-  //   const response = await fetch('https://your-cloud-function-url/createPaymentIntent', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ cart, tip, taxRate: TAXRATE }),
-  //   });
-
-  //   const { clientSecret } = await response.json();
-  //   return clientSecret;
-  // };
-
-  // const initializePaymentSheet = async () => {
-  //   const clientSecret = await fetchPaymentSheetParams();
-
-  //   const { error } = await initPaymentSheet({
-  //     paymentIntentClientSecret: clientSecret,
-  //     merchantDisplayName: 'Your Merchant Name',
-  //   });
-
-  //   if (!error) {
-  //     await presentPaymentSheet();
-  //     handleCheckout();
-  //   } else {
-  //     Alert.alert('Error', error.message);
-  //   }
-  // };
-
   const handleCheckout = async () => {
+    if (!selectedPaymentMethod) {
+      Alert.alert('Error', 'Please select a payment method.');
+      return;
+    }
     const currentUser = auth.currentUser;
+
     if (!currentUser) {
       Alert.alert('Error', 'User not logged in.');
       return;
@@ -87,7 +74,7 @@ const CheckoutScreen = ({ navigation }) => {
 
     const userOrderData = {
       bar: barId,
-      paymentMethod: selectedPaymentMethod ? selectedPaymentMethod.name : 'Stripe',
+      paymentMethod: selectedPaymentMethod.name,
       totalAmount: calculateTotalAmount(),
       timestamp: Timestamp.now(),
       items: cart.map(item => ({
@@ -100,10 +87,9 @@ const CheckoutScreen = ({ navigation }) => {
       tax: taxAmount,
       fulfilled: false,
     };
-
     const barOrderData = {
       customer: currentUser.uid,
-      paymentMethod: selectedPaymentMethod ? selectedPaymentMethod.name : 'Stripe',
+      paymentMethod: selectedPaymentMethod.name,
       totalAmount: calculateTotalAmount(),
       timestamp: Timestamp.now(),
       items: cart.map(item => ({
@@ -122,11 +108,12 @@ const CheckoutScreen = ({ navigation }) => {
       const barDocRef = doc(db, 'bars', barId);
       
       const ordersCollectionRef = collection(userDocRef, 'OrderHist');
-      const barOrdersCollectionRef = collection(barDocRef, 'orders')
+      const barOrdersCollectionRef = collection(barDocRef, 'orders');
       
       await addDoc(ordersCollectionRef, userOrderData);
       await addDoc(barOrdersCollectionRef, barOrderData);
 
+      // Create PaymentIntent on your backend
       const response = await axios.post('https://us-central1-orderapp-4b33a.cloudfunctions.net/createPaymentIntent', {
         cart: userOrderData.items,
         tip: userOrderData.tip,
@@ -135,11 +122,22 @@ const CheckoutScreen = ({ navigation }) => {
 
       const { clientSecret } = response.data;
 
-      Alert.alert('Success', `Payment of $${calculateTotalAmount().toFixed(2)} made using ${selectedPaymentMethod.name}`);
-      console.log("success placing order")
-      navigation.navigate('Confirmation', { userOrderData, clientSecret });
+      const { error } = await initPaymentSheet({
+        paymentIntentClientSecret: clientSecret,
+      });
+
+      if (!error) {
+        const { error: paymentError } = await presentPaymentSheet();
+        
+        if (paymentError) {
+          Alert.alert(`Error code: ${paymentError.code}`, paymentError.message);
+        } else {
+          Alert.alert('Success', 'Your order is confirmed!');
+          navigation.navigate('Confirmation', { userOrderData });
+        }
+      }
     } catch (error) {
-      console.error('Error adding document: ', error);
+      console.error('Error during checkout: ', error);
       Alert.alert('Error', 'There was an issue processing your order. Please try again.');
     }
   };
@@ -148,7 +146,7 @@ const CheckoutScreen = ({ navigation }) => {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
         <Text style={styles.title}>Checkout</Text>
-
+        
         <Text style={styles.subtitle}>Select Payment Method:</Text>
         {paymentMethods.map((method) => (
           <TouchableOpacity
@@ -174,7 +172,7 @@ const CheckoutScreen = ({ navigation }) => {
           value={tip}
           onChangeText={handleTipChange}
         />
-
+        
         <View>
           <Text style={styles.subtitle}>Subtotal: ${subtotal.toFixed(2)}</Text>
           <Text style={styles.subtitle}>Tax: ${taxAmount.toFixed(2)}</Text>
